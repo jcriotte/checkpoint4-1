b061ca35efff232dce19c0e481435ca9f5150518
jgautrais <j.gautrais@gmail.com>
2022-02-03 14:20:32 +0100

    fixtures update

--- src/DataFixtures/BookingFixtures.php
+++ src/DataFixtures/BookingFixtures.php
@@ -26,7 +26,7 @@ class BookingFixtures extends Fixture implements DependentFixtureInterface
         for ($i = 0; $i < self::BOOKINGS; $i++) {
             $booking = new Booking();
 
-            $userId = rand(0, 200);
+            $userId = rand(0, 199);
             $courtId = rand(1, 5);
 
             $date = rand($minDate, $maxDate);
